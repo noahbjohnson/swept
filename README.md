d72d875417bf780e78b0b3ce6f2c7d2332215942
Noah B Johnson <noah@heartfelt.tech>
2020-06-27 00:08:03 -0500

    parsed frames stream to influx

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # swept
 Hackrf sweep stream processing
+
+## Getting Started
+
+- [Install golang](https://golang.org/doc/install)
+
+- Install influxdb
+
+```shell script
+brew update
+brew install influxdb
+```
+
+```shell script
+influxd
+```
+
+```shell script
+influx
+```
+```sql
+create database rf
+```
--- main.go
+++ main.go
@@ -3,46 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	influxdb2 "github.com/influxdata/influxdb-client-go"
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
-
-var samples = 0
-
-func sweep(widthHz int) {
-	// construct arguments array for the sweep call
-	var arguments []string
-	// one-shot mode (single sweep)
-	arguments = append(arguments, "-1")
-	// enable rx amplifier
-	arguments = append(arguments, "-a 1")
-	// bin width in hertz
-	arguments = append(arguments, fmt.Sprintf("-w %v", widthHz))
-
-	// call the sweep with arguments
-	cmd := exec.Command("hackrf_sweep", arguments...)
-
-	// create new standard out pipe for the sweep
-	out, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-
-	// fire off sweep
-	err = cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	// line parser for the stdout
-	scanner := bufio.NewScanner(out)
-
-	// split lines
-	for scanner.Scan() {
-		fmt.Println(splitRow(scanner.Text()))
-	}
-}
 
 /*
 Parses a string into an integer representing hertz
@@ -68,38 +34,85 @@ func calculateBinRange(hzLow int, hzHigh int, hzBinWidth int, binNum int) [2]int
 	return [2]int{low, high}
 }
 
-// split row into multiple single-bin rows
-// [date, time, hz_low, hz_high, hz_bin_width, num_samples, bin1dB, bin2dB, bin3dB...]
-// bin1 frequency range = (hz_low) > x < (hz_low + hz_bin_width)
-// bin2 frequency range = (hz_low + hz_bin_width) > x < (hz_low + hz_bin_width * 2)
-// etc...
-func splitRow(rowString string) [][6]string {
-	var row = strings.Split(rowString, ", ")
-	// processed rows, one bin each
-	// [date, time, hz_low, hz_high, samples (total samples / n-bins), db]
-	var binRows [][6]string
+func main() {
+	var client = influxdb2.NewClientWithOptions("http://localhost:8086", "", influxdb2.DefaultOptions().SetLogLevel(3))
+	var writeApi = client.WriteApi("", "rf")
+	errorsCh := writeApi.Errors()
+	go func() {
+		for err := range errorsCh {
+			fmt.Printf("write error: %s\n", err.Error())
+		}
+	}()
 
-	// 6 fields before the data values begin
-	var numBins = len(row) - 6
-	samples = samples + numBins
-	var hzLow = frequencyStringToInt(row[2])
-	var hzHigh = frequencyStringToInt(row[3])
-	var hzBinWidth = frequencyStringToInt(row[4])
-	var samples = frequencyStringToInt(row[5])
+	// construct arguments array for the sweep call
+	var arguments []string
+	// one-shot mode (single sweep)
+	//arguments = append(arguments, "-1")
+	// enable rx amplifier
+	arguments = append(arguments, "-a 1")
+	// bin width in hertz
+	//arguments = append(arguments, fmt.Sprintf("-w %v", 1000000))
 
-	for i := 0; i < numBins; i++ {
-		var binRange = calculateBinRange(hzLow, hzHigh, hzBinWidth, i)
-		var binRowIndex = i + 6
-		var dataBinRow = [6]string{row[0], row[1], strconv.Itoa(binRange[0]), strconv.Itoa(binRange[1]), strconv.Itoa(samples / numBins), row[binRowIndex]}
-		binRows = append(binRows, dataBinRow)
+	// call the sweep with arguments
+	cmd := exec.Command("hackrf_sweep", arguments...)
+
+	// create new standard out pipe for the sweep
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
 	}
 
-	return binRows
-}
+	// fire off sweep
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
 
-func main() {
-	// TODO:
-	//   check hackrf with hackrf_info
-	//   support rtl-sdr
-	sweep(1000000)
+	// line parser for the stdout
+	scanner := bufio.NewScanner(out)
+	count := 0
+
+	/*
+		split row into multiple single-bin rows and insert them into influx
+		[date, time, hz_low, hz_high, hz_bin_width, num_samples, bin1dB, bin2dB, bin3dB...]
+		bin1 frequency range = (hz_low) > x < (hz_low + hz_bin_width)
+		bin2 frequency range = (hz_low + hz_bin_width) > x < (hz_low + hz_bin_width * 2)
+		etc...
+	*/
+	for scanner.Scan() {
+		rowString := scanner.Text()
+		var row = strings.Split(rowString, ", ")
+		var numBins = len(row) - 6
+		count = count + numBins
+		var hzLow = frequencyStringToInt(row[2])
+		var hzHigh = frequencyStringToInt(row[3])
+		var hzBinWidth = frequencyStringToInt(row[4])
+		var samples = frequencyStringToInt(row[5])
+		for i := 0; i < numBins; i++ {
+			var binRange = calculateBinRange(hzLow, hzHigh, hzBinWidth, i)
+			var binRowIndex = i + 6
+			var db float64
+			var datetime time.Time
+			var err error
+			datetime, err = time.Parse(time.RFC3339, row[0]+"T"+row[1]+"Z")
+			if err != nil {
+				panic(err)
+			}
+			db, err = strconv.ParseFloat(row[binRowIndex], 64)
+			if err != nil {
+				panic(err)
+			}
+			p := influxdb2.NewPoint("rfdb",
+				map[string]string{"hzLow": strconv.Itoa(binRange[0]),
+					"hzHigh": strconv.Itoa(binRange[1])},
+				map[string]interface{}{
+					"samples": samples / numBins,
+					"db":      db},
+				datetime)
+			writeApi.WritePoint(p)
+			//fmt.Println(count)
+		}
+	}
+	writeApi.Flush()
+	client.Close()
 }
