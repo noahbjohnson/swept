4dae08910a7a52f1ad12d1553a77094c2ea9d073
noahbjohnson <noah@noahbjohnson.net>
2020-07-18 12:06:52 -0500

    row struct

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -51,7 +52,7 @@ func errPanic(err error) {
 	}
 }
 
-func scanRow(scanner *bufio.Scanner) (rows [][]string, runtime time.Duration) {
+func scanRow(scanner *bufio.Scanner) (rows []Row, runtime time.Duration) {
 	// Timer
 	start := time.Now()
 	defer func() { runtime = time.Since(start) }()
@@ -65,9 +66,18 @@ func scanRow(scanner *bufio.Scanner) (rows [][]string, runtime time.Duration) {
 	return
 }
 
+type Row struct {
+	Id        int64 `xorm:"pk autoincr"`
+	HzLow     int   `xorm:"index"`
+	HzHigh    int   `xorm:"index"`
+	Decibels  float64
+	N         int
+	Timestamp time.Time
+}
+
 // Break one row with multiple bin values into multiple rows with one bin each
 // Append extracted rows to row array
-func parseRow(rows [][]string, rowString string) [][]string {
+func parseRow(rows []Row, rowString string) []Row {
 	var row = strings.Split(rowString, ", ")
 	var numBins = len(row) - 6
 	var samples = frequencyStringToInt(row[5])
@@ -80,34 +90,44 @@ func parseRow(rows [][]string, rowString string) [][]string {
 		var binRowIndex = i + 6
 		parsedTime, err := time.Parse(time.RFC3339, row[0]+"T"+row[1]+"Z")
 		errPanic(err)
-		decibels := row[binRowIndex]
-		insertRow := []string{
-			strconv.Itoa(low),
-			strconv.Itoa(high),
-			decibels,
-			strconv.Itoa(samples),
-			parsedTime.String()}
+		decibels, err := strconv.ParseFloat(row[binRowIndex], 64)
+		errPanic(err)
+		insertRow := Row{
+			HzLow:     low,
+			HzHigh:    high,
+			Decibels:  decibels,
+			N:         samples,
+			Timestamp: parsedTime,
+		}
 		rows = append(rows, insertRow)
 	}
 	return rows
 }
 
 func main() {
-	var laps float64
+	var (
+		laps float64
+		rows []Row
+		err  error
+	)
+
 	// Setup Command
 	cmd := exec.Command(sweepAlias, constructSweepArgs(false, 1000000)...)
 	out, err := cmd.StdoutPipe()
-	defer func() {
-		err = out.Close()
-		errPanic(err)
-	}()
 	errPanic(err)
+
+	defer func() {
+		_ = out.Close()
+	}()
+
+	//engine, err := xorm.NewEngine("sqlite3", "./test.db")
+	//errPanic(err)
+
+	scanner := bufio.NewScanner(out)
 	err = cmd.Start()
 	errPanic(err)
 
-	scanner := bufio.NewScanner(out)
-	var rows [][]string
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 100000000; i++ {
 		newRows, duration := scanRow(scanner)
 		if len(newRows) < 1 {
 			break
